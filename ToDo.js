1b4a9addd46b82e78c256a83b6f8f3266232f4b3
smrutee <138093335+smrutee20@users.noreply.github.com>
2024-03-17 16:32:16 +0530

    Add files via upload

---
+++ ToDo.js
@@ -0,0 +1,35 @@
+let todo= [];
+
+let req = prompt("please enter your request");
+
+while(true){
+    if(req == "quit"){
+        console.log("quitting app");
+        break;
+    }
+
+    if(req == "list"){
+        console.log("----------------");
+        for(let i=0; i<todo.length; i++){
+            console.log(i, todo[i]);
+        }
+        console.log("----------------");
+    }
+    else if(req == "add"){
+        let task = prompt("please enter the task you want to add : ");
+        todo.push(task);
+        console.log("task added");
+
+    }
+    else if( req == "delete"){
+        let idx = prompt("please enter the task you want to delete: ");
+        todo.slice(idx, 1);
+        console.log("task deleted");
+    }
+    else{
+        alert("Wrong request!");
+    }
+
+    req = prompt("please enter your request");
+    
+}
